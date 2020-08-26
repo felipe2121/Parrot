4d2cbdd102517b72c56f65e696edb1b1976c73b6
Felipe Alves Holanda <felipe.holanda12@yahoo.com.br>
2020-08-26 19:51:11 -0300

    Commit inicial

---
+++ app/src/main/java/br/felipe/parrot/activity/ParrotActivity.kt
@@ -0,0 +1,13 @@
+package br.felipe.parrot.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.felipe.parrot.R
+
+class ParrotActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_parrot)
+    }
+}
