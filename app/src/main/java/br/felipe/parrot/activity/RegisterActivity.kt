33386fb9c694ad387e404c6fd2712129926c7a74
felipeholanda <felipe.holanda12@yahoo.com.br>
2020-08-27 18:43:02 -0300

    SplashScreen. Icone.

---
+++ app/src/main/java/br/felipe/parrot/activity/RegisterActivity.kt
@@ -0,0 +1,11 @@
+package br.felipe.parrot.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RegisterActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
