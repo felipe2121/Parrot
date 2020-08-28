fe1d559deea01078cd2cd1afe6e4943934c3cdf0
felipeholanda <felipe.holanda12@yahoo.com.br>
2020-08-28 17:31:29 -0300

    Themes abd fonts.

--- app/src/main/java/br/felipe/parrot/activity/RegisterActivity.kt
+++ app/src/main/java/br/felipe/parrot/activity/RegisterActivity.kt
@@ -2,10 +2,21 @@ package br.felipe.parrot.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import br.felipe.parrot.R
+import kotlinx.android.synthetic.main.register_activity.*
 
 class RegisterActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.register_activity)
+
+        setUpUI()
+    }
+
+    private fun setUpUI() {
+        setSupportActionBar(register_toolBar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
     }
 }
